d1752bf395c7a8c535d7adaac6626d429e5afcad
Brian Nash <bcnjr5@gmail.com>
2016-08-22 10:04:50 -0400

    Added racist stuff.

--- src/p3600/main_menu/new_game/race.lua
+++ src/p3600/main_menu/new_game/race.lua
@@ -2,7 +2,7 @@ return function()
   p3600.push_state()
 
   p3600.clear_love_callbacks()
-  p3600.slowness = 0.5
+  p3600.slowness = 0.1
   p3600.state = {
     r = p3600.state_stack.state.r,
     race = p3600.state_stack.state.race,
@@ -12,12 +12,30 @@ return function()
   p3600.keypressed = function(key)
     local tbl = {
       ['left'] = function()
+        repeat
+          if (p3600.state.race > 0) then
+            p3600.state.race = p3600.state.race - 1
+          else
+            p3600.state.race = #p3600.state.r
+          end
+        until (p3600.state.r[p3600.state.race].playable)
+        p3600.state.changed = true
       end,
 
       ['right'] = function()
+        repeat
+          if (p3600.state.race < #p3600.state.r) then
+            p3600.state.race = p3600.state.race + 1
+          else
+            p3600.state.race = 0
+          end
+        until (p3600.state.r[p3600.state.race].playable)
+        p3600.state.changed = true
       end,
 
       ['return'] = function()
+        p3600.state_stack.state.race = p3600.state.race
+        p3600.pop_state()
       end,
 
       ['escape'] = function()
@@ -32,7 +50,9 @@ return function()
   p3600.draw = function()
     if (p3600.state.changed) then
       love.graphics.clear()
-      p3600.display.print(4, 4, 'Race: '..r[p3600.state.race].singular)
+      p3600.display.print(1, 3,
+                          'Race: '..p3600.state.r[p3600.state.race].singular)
+      p3600.display.print(3, 1, p3600.state.r[p3600.state.race].description)
       p3600.display.changed = true
       p3600.state.changed = false
     end
--- src/p3600/race.lua
+++ src/p3600/race.lua
@@ -3,45 +3,102 @@ return {
     singular  = 'human',
     plural    = 'humans',
     adjective = 'human',
+
     playable  = true,
+    description = [[
+A human.
+'nuff said.
+
+Has the most potential, but starts off weak.]],
   },
 
   [1] = {
     singular  = 'dwarf',
     plural    = 'dwarves',
     adjective = 'dwarven',
+
     playable  = true,
+    description = [[
+ A short, sturdy creature fond of drink and
+ industry.  -- The First Toad
+
+Short, extremely resilient, and a little crazy.
+Dwarves are some of the world's greatest
+artisans, smiths, and engineers, but they have
+a tendency to start wars and set forests on
+fire.
+
+Slow, hairy, and usually drunk.]],
   },
 
   [2] = {
     singular  = 'elf',
     plural    = 'elves',
     adjective = 'elven',
+
     playable  = true,
+    description = [[
+The elves are immortal beings that live in
+forests and do elf things like growing trees
+and hating dwarves.
+
+Tall, fast, weak, fragile.]],
   },
 
   [3] = {
     singular  = 'kobold',
     plural    = 'kobolds',
     adjective = 'small',
+
     playable  = true, -- joke
+    description = [[
+Annoying vermin that steal stuff.
+They have no language, and nobody feels
+like teaching them to speak anyway.
+
+Tiny, cowardly, stealthy kleptomaniacs.]],
   },
 
   [4] = {
     singular  = 'bragasian',
     plural    = 'bragasians',
     adjective = 'bragasian',
+
     playable  = true,
+    description = [[
+Bragasians are actually just humans.
+They usually posess some form of psychic
+abilities, which leads to most of the other
+races fearing them. Live far away from other
+races, but are accepting of outsiders. (Mostly
+because they know if you are planning something
+bad.)
+
+Politicians hate them for some reason.]],
   },
 
   [5] = {
     singular  = 'fairy',
     plural    = 'fairies',
     adjective = 'fey',
+
     playable  = true,
+    description = [[
+Tiny winged humanoids with a natural affinity
+for magic. They are easily defeated by a stiff
+breeze, but their size allows them to go
+unnoticed most of the time.
+
+4 inches tall, can fly, can glow.
+
+They are capable of increasing their size
+to about 5 feet, but this form takes a lot of
+energy and can not be maintained for long.
+
+They are infamous pranksters.]],
   },
 
-  [6] = {
+  [6] = { -- fairy, alternate form
     singular  = 'faerie',
     plural    = 'faeries',
     adjective = 'fey',
@@ -51,10 +108,21 @@ return {
     singular  = 'mamkute',
     plural    = 'mamkutes',
     adjective = 'draconic',
+
     playable  = true,
+    description = [[
+Long ago, dragons ruled the skies.
+During a great event, all dragons slowly began
+going "feral". In order to retain their sanity,
+many dragons sealed their forms away and took
+on a human appearance.
+
+Extremely long lifespan, strong, can use magic.
+Can temporarily turn into a dragon using special
+stones.]],
   },
 
-  [8] = {
+  [8] = { -- mamkute, alternate form
     singular  = 'dragon',
     plural    = 'dragons',
     adjective = 'draconic',
@@ -64,10 +132,20 @@ return {
     singular  = 'kitsune',
     plural    = 'kitsunes',
     adjective = 'otherworldly',
+
     playable  = true,
+    description = [[
+A race of eldrich, fox-like humanoids.
+They live for 900 years, and posess ancient
+knowledge beyond mortal ken.
+They gain another tail every 100 years, and
+9-tailed kitsunes are exceptionally powerful;
+they can even attain immortality.
+
+Can turn into a fox.]],
   },
 
-  [10] = {
+  [10] = { -- kitsune, alternate form
     singular  = 'fox',
     plural    = 'foxes',
     adjective = 'vulpine',
@@ -77,38 +155,111 @@ return {
     singular  = 'doppelganger',
     plural    = 'doppelgangers',
     adjective = 'strange',
+
     playable  = true,
+    description = [[
+Shapeshifters.
+
+They can take the form of anything they touch.
+
+They must concentrate to maintain a form, and
+they all have unique forms that they revert
+to when they are not shifted.
+
+It is impossible to identify a shapeshifter
+without seeing one transform, so many live
+amongst the other races.
+
+Like bragasians, they can pose a threat to
+the powerful, so they are often prosecuted
+if they are discovered.]],
   },
 
   [12] = {
     singular  = 'goblin',
     plural    = 'goblins',
     adjective = 'ugly',
+
     playable  = true,
+    description = [[
+A goblin is basically a green dwarf.
+They are even stronger than dwarves, but
+they lack the dwarves' dexterity.
+
+Usually cruel by nature, very few are actually
+peaceful. The few peaceful goblins are rather
+"creative", and are good at tinkering with
+things. A goblin tinkerer with dwarven technology
+is a force to be reckoned with.]],
   },
 
   [13] = {
     singular  = 'incubus',
     plural    = 'incubi',
     adjective = 'demonic',
-    playable  = true,
     limit_sex = 1,
+
+    playable  = true,
+    description = [[
+Long ago, the demons created a race of hellish
+creatures that could survive in the light and
+set them loose on the surface to sow pain and
+suffering. This plan backfired, as when these
+creatures looked upon the light, they fell in
+love with the world.
+
+While they still have demonic tendencies, they
+are not inherently evil.
+
+They are immune to most fire.
+
+The incubus is the male variant.]],
   },
 
   [14] = {
     singular  = 'succubus',
     plural    = 'succubi',
     adjective = 'demonic',
-    playable  = true,
     limit_sex = 2,
+
+    playable  = true,
+    description = [[
+Long ago, the demons created a race of hellish
+creatures that could survive in the light and
+set them loose on the surface to sow pain and
+suffering. This plan backfired, as when these
+creatures looked upon the light, they fell in
+love with the world.
+
+While they still have demonic tendencies, they
+are not inherently evil.
+
+They are immune to most fire.
+
+The succubus is the female variant.]],
   },
 
   [15] = {
     singular  = 'omnibus',
     plural    = 'omnibi',
     adjective = 'demonic',
-    playable  = true,
     limit_sex = 3,
+
+    playable  = true,
+    description = [[
+Long ago, the demons created a race of hellish
+creatures that could survive in the light and
+set them loose on the surface to sow pain and
+suffering. This plan backfired, as when these
+creatures looked upon the light, they fell in
+love with the world.
+
+While they still have demonic tendencies, they
+are not inherently evil.
+
+They are immune to most fire.
+
+The omnibus is the *ahem* "special" variant.]],
   },
 
   [16] = {
