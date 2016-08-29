0ef994928ff94bfe169bfb24bd8286f4cd1b21fd
Brian Nash <bcnjr5@gmail.com>
2016-08-29 10:20:01 -0400

    Switched to A* pathfinder

--- src/p3600/pathfinder.lua
+++ src/p3600/pathfinder.lua
@@ -1,7 +1,7 @@
 return function(grid, sx, sy, dx, dy)
   if (grid._jumper == nil) then
     local g = require('jumper.grid')(grid)
-    grid._jumper = require('jumper.pathfinder')(g, 'JPS', 0)
+    grid._jumper = require('jumper.pathfinder')(g, 'ASTAR', 0)
   end
 
   return grid._jumper:getPath(sx, sy, dx, dy)
