e45d669fbddec0596c4df935f7ea60ffe9b1a936
Brian Nash <bcnjr5@gmail.com>
2016-09-03 18:44:50 -0400

    Began work on inventory

---
+++ src/p3600/item/sao_armor.lua
@@ -0,0 +1,20 @@
+return {
+  id = 'sao_armor',
+  description = {
+    "Saoirse's preferred \"armor\".",
+    '',
+    'A light leather "bandeau" and shorts',
+    'complete with footwraps.',
+    'There appears to be some type of',
+    'chainmail inside the leather.',
+    '',
+    'Meant more for evasion than defense.',
+  },
+  equipment = {
+    defense = 8.02,
+    coverage_skin = 0.4,
+    coverage_light = 0.4,
+    freedom = 0.9,
+    weight = 0.3,
+  },
+}
