9fc85669249309d0c8be801ba2173687b4c22f47
Christopher Clements <bcnjr5@gmail.com>
2017-01-15 20:13:38 -0500

    Switched to text-based images

--- src/conf.lua
+++ src/conf.lua
@@ -33,7 +33,7 @@ function love.conf(t)
   t.version = "0.10.1"                -- The LÖVE version this game was made for (string)
   t.accelerometerjoystick = false     -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
   t.window.title = "p3600"            -- The window title (string)
-  t.window.icon = '/data/icon.png'    -- Filepath to an image to use as the window's icon (string)
+  -- t.window.icon should be unset; the icon is handled elsewhere
   t.window.resizable = true           -- Let the window be user-resizable (boolean)
 
   -- optional modules
